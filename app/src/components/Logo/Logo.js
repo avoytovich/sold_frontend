28e407b7b5c34271ac7400db65ef19799a702daf
levanwork <levanwork@ukr.net>
2017-11-07 23:24:45 +0200

    Created SignUp Component

--- app/src/components/Logo/Logo.js
+++ app/src/components/Logo/Logo.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 import { Col, Button } from 'react-bootstrap';
 import './Logo.css';
 
 export class Logo extends React.Component {
+
+  signUp(e) {
+    e.preventDefault();
+    browserHistory.push('/signup');
+  }
+
   render() {
     return (
       <div id='logo'>
-        <Button bsStyle='success'>Sign up</Button>
+        <Button
+          bsStyle='success'
+          onClick={this.signUp}
+        >Sign up</Button>
         <img className='logo' src={require('./../../../assets/sold.png')} alt='logo' />
       </div>
     );
--- app/src/components/index.js
+++ app/src/components/index.js
@@ -1,4 +1,4 @@
 export { About } from './About/About';
 export { Logo } from './Logo/Logo';
 export Login from './Login/Login';
-export { SignUp } from './SignUp/SignUp';
+export SignUp from './SignUp/SignUp';
