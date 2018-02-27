a6128c822a62cefc340885a63b1ad836865c0f69
Andrii <andrew.voytovich@boost.solutions>
2018-02-27 19:40:21 +0200

    working space 0

--- app/src/components/Dashboard/Dashboard.js
+++ app/src/components/Dashboard/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Grid, Row, Col, Button } from 'react-bootstrap';
+import { Grid, Row, Col, Button, PanelGroup, Panel } from 'react-bootstrap';
 import { getMyProposals } from './getMyProposalsActions.js';
 import { getMyOffersProposal } from './retrieveOffersProposalActions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import './Dashboard.css';
 
 export class Dashboard extends React.Component {
 
@@ -45,19 +46,53 @@ export class Dashboard extends React.Component {
             </div>
           </Col>
           <Col xs={4} sm={4} md={4}>
-            <h3>Its your proposals :)</h3>
+            <h3 className="proposalMy">your's proposals</h3>
             {proposalsMy && proposalsMy.map((proposalMy, id) => {
+              var defaultActiveKey = proposalsMy.length;
               return (
-                <Button
+
+                <PanelGroup key={id} accordion id="accordion-example">
+                  <Panel eventKey={id}>
+                    <Panel.Heading>
+                      <Panel.Title toggle>{proposalMy}</Panel.Title>
+                    </Panel.Heading>
+                    <Panel.Body collapsible>
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+                      richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard
+                      dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf
+                      moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                      assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
+                      wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                      butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim
+                      aesthetic synth nesciunt you probably haven't heard of them accusamus
+                      labore sustainable VHS.
+                    </Panel.Body>
+                  </Panel>
+                </PanelGroup>
+
+                /*<Tabs
+                  key={id}
+                  defaultActiveKey={defaultActiveKey}
+                  id="uncontrolled-tab-example"
                   onClick={this.myOffersProposal.bind(this, proposalMy)}
-                  key={id} >
-                  {proposalMy}
-                </Button>
+                >
+                    <Tab eventKey={id} title={proposalMy} >
+                      <h3 className="offersByProposal">offers which was sent for your consideration</h3>
+                      {myOffersByProposal && myOffersByProposal.map((offer, id) => {
+                        return (
+                          <p
+                            key={id}
+                            className="offersByProposal"
+                            /!*onClick={this.myOffersProposal.bind(this, proposalMy)}*!/
+                          >
+                            {offer.title}
+                          </p>
+                        );
+                      })}
+                    </Tab>
+                </Tabs>*/
               );
             })}
-          </Col>
-          <Col xs={4} sm={4} md={4}>
-
           </Col>
         </Row>
       </Grid>
