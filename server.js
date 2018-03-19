8e0d2d1f605ce39ec86f11432d5a1028ae81125a
Andrii <andrew.voytovich@boost.solutions>
2018-03-19 15:57:11 +0200

    atempt_1 deployed on heroku

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3050;
 
-app.use(express.static('build'));
+//app.use(express.static('build'));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'));
+  res.sendFile(path.join(__dirname, 'app/src/index.html'));
 });
 
 app.listen(PORT, () => console.log('Listening server on port ' + PORT));
